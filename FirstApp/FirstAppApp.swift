94b3ab830ca6e462049e518812a1af277268bbfa
Mithra Singam <mithrasvs@gmail.com>
2021-02-20 10:52:11 -0800

    initial commiy

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Singam on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
